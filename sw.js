8e5901c827336f0c9b590dfdfa99288d8dbec3fe
Felix Kohlgrüber <felix.kohlgrueber@gmail.com>
2020-05-14 10:50:22 +0200

    maybe serviceworker

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ var cacheName = 'hello-pwa';
 var filesToCache = [
   '/',
   '/index.html',
-  '/css/style.css',
-  '/js/main.js'
+  '/style.css'
+  //'/js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
